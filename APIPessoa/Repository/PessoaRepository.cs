b38afb74d3c17f6bf821eb146a95a3d14a9a9a27
freireejp <efreire@id.uff.br>
2023-02-07 14:23:24 -0300

    inicial

---
+++ APIPessoa/Repository/PessoaRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using MySqlConnector;
+
+namespace APIPessoa.Repository
+{
+    public class PessoaRepository
+    {
+        public List<Pessoa> SelecionarPessoas()
+        {
+            string query = "SELECT * FROM Pessoa";
+
+            string stringConnection = Environment.GetEnvironmentVariable("DATABASE_CONFIG");
+
+            using MySqlConnection conn = new(stringConnection);
+
+            return conn.Query<Pessoa>(query).ToList();
+        }
+
+        public void InserirPessoa(Pessoa pessoa)
+        {
+            string query = $"INSERT INTO Pessoa (nome, dataNascimento, quantidadeFilhos)" +
+                $"VALUES ('{pessoa.Nome}', '{pessoa.DataNascimento.ToString("yyyy-MM-dd")}', {pessoa.QuantidadeFilhos})";
+
+            string stringConnection = Environment.GetEnvironmentVariable("DATABASE_CONFIG");
+
+            using MySqlConnection conn = new(stringConnection);
+
+            conn.Execute(query);
+        }
+    }
+}
